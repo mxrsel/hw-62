f4cb0e99f2a8ab9e146b2369baf1aeb723eaf890
mxrsel <marsel14082006@gmail.com>
2024-06-23 18:17:00 +0600

    component About was finished

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="container">
+            <div className="mt-4">
+                <h2>About Fixed Gear Bikes</h2>
+                <p>
+                    Fixed Gear Bikes is dedicated to the simplicity and pleasure of riding fixed gear bicycles. We provide cyclists with a platform where they can do everything for fixed gear.                </p>
+            </div>
         </div>
     );
 };
