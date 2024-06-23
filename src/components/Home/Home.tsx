97d27a67aca111cb4f7bbc825024d93fe8211d8d
mxrsel <marsel14082006@gmail.com>
2024-06-23 18:08:09 +0600

    finished page Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-
+import bike from 'src/assets/buke.jpg'
 const Home = () => {
     return (
-        <div>
-            
+        <div className="container">
+            <div className="card">
+                <img src={bike} className="card-img-top" alt="Fixed Gear Bike" />
+                <div className="card-body">
+                    <h5 className="card-title">Welcome to Fixed Gear Bikes</h5>
+                    <p className="card-text">
+                        Explore the world of fixed gear bikes with us. Discover unique rides and stories from passionate cyclists.
+                    </p>
+                    <a href="/" className="btn btn-primary">Learn More</a>
+                </div>
+            </div>
         </div>
     );
 };
